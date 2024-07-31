ffc2483b1ddde9da54c3878d513be77d8ad7a6e9
Mr.Mao <951416545@qq.com>
2024-07-31 15:13:34 +0800

    feat: update storage driver base

--- packages/harsta/node/utils/storage.ts
+++ packages/harsta/node/utils/storage.ts
@@ -4,5 +4,5 @@ import fsDriver from 'unstorage/drivers/fs'
 import { userRoot } from '../constants/root'
 
 export const storage = createStorage({
-  driver: fsDriver({ base: path.resolve(userRoot, '.harsta/deployments') }),
+  driver: fsDriver({ base: path.resolve(userRoot, '.harsta/storage') }),
 })

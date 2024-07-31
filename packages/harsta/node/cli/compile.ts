e5b8668f48a6dbbb00f44118c8e58cec7cf53eb5
Mr.Mao <951416545@qq.com>
2024-07-31 11:37:08 +0800

    fix: resolve fragments paths error

--- packages/harsta/node/cli/compile.ts
+++ packages/harsta/node/cli/compile.ts
@@ -1,13 +1,13 @@
-// import path from 'pathe'
-// import fs from 'fs-extra'
-// import { glob, runTypeChain } from 'typechain'
+import path from 'pathe'
+import fs from 'fs-extra'
+import { glob, runTypeChain } from 'typechain'
 import type { Argv } from 'yargs'
-// import consola from 'consola'
-// import { dim } from 'kolorist'
-// import { resolveUserPath } from '../utils'
-// import type { Chain } from '../types'
-// import { clientRoot, packRoot, userConf, userRoot } from '../constants'
-// import { exec, hardhatBinRoot, resolveFragmentsPaths, resolveUserAddresses, tscBinRoot } from './utils'
+import consola from 'consola'
+import { dim } from 'kolorist'
+import { resolveUserPath } from '../utils'
+import type { Chain } from '../types'
+import { clientRoot, packRoot, userConf, userRoot } from '../constants'
+import { exec, hardhatBinRoot, resolveFragmentsPaths, resolveUserAddresses, tscBinRoot } from './utils'
 
 export function registerCompileCommand(cli: Argv) {
   cli.command(
@@ -25,240 +25,240 @@ export function registerCompileCommand(cli: Argv) {
         type: 'boolean',
       })
       .help(),
-    async () => {
-      // const generateRoot = path.resolve(packRoot, './generated')
+    async (args) => {
+      const generateRoot = path.resolve(packRoot, './generated')
 
-      // await fs.remove(path.resolve(packRoot, './contracts'))
-      // await fs.remove(path.resolve(generateRoot, './contracts'))
-      // await fs.remove(path.resolve(generateRoot, './fragments'))
-      // if (userConf.paths?.fragments) {
-      //   await fs.remove(path.join(resolveUserPath(userConf.paths.fragments)!, './@openzeppelin'))
-      //   await fs.remove(path.join(resolveUserPath(userConf.paths.fragments)!, './contracts'))
-      // }
+      await fs.remove(path.resolve(packRoot, './contracts'))
+      await fs.remove(path.resolve(generateRoot, './contracts'))
+      await fs.remove(path.resolve(generateRoot, './fragments'))
+      if (userConf.paths?.fragments) {
+        await fs.remove(path.join(resolveUserPath(userConf.paths.fragments)!, './@openzeppelin'))
+        await fs.remove(path.join(resolveUserPath(userConf.paths.fragments)!, './contracts'))
+      }
 
-      // await fs.copy(
-      //   path.resolve(userRoot, './contracts'),
-      //   path.resolve(packRoot, './contracts'),
-      // )
-      // args.clean && exec(`node ${hardhatBinRoot} clean`)
-      // exec(`node ${hardhatBinRoot} compile`)
-      // exec(`node ${hardhatBinRoot} export-abi`)
+      await fs.copy(
+        path.resolve(userRoot, './contracts'),
+        path.resolve(packRoot, './contracts'),
+      )
+      args.clean && exec(`node ${hardhatBinRoot} clean`)
+      exec(`node ${hardhatBinRoot} compile`)
+      exec(`node ${hardhatBinRoot} export-abi`)
 
-      // function presolve(_path: string) {
-      //   return path.resolve(generateRoot, _path)
-      // }
+      const allFiles = glob(userRoot, ['./config/fragments/*.json'])
+      await runTypeChain({
+        cwd: userRoot,
+        allFiles,
+        filesToProcess: allFiles,
+        target: 'ethers-v6',
+        outDir: presolve('./typechains/extends'),
+      })
 
-      // const [fragmentsPaths, fragmentsExtendsPaths] = resolveFragmentsPaths()
+      function presolve(_path: string) {
+        return path.resolve(generateRoot, _path)
+      }
 
-      // await Promise.all([
-      //   buildAddresses(),
-      //   buildChains(),
-      //   buildFragments(),
-      //   buildContractsExtends(),
-      //   buildContracts(),
-      // ])
+      const [fragmentsPaths, fragmentsExtendsPaths] = resolveFragmentsPaths()
 
-      // await buildTypes()
+      await Promise.all([
+        buildAddresses(),
+        buildChains(),
+        buildFragments(),
+        buildContractsExtends(),
+        buildContracts(),
+      ])
 
-      // const defaultOutput = clientRoot ? path.resolve(clientRoot, 'build') : path.resolve(userRoot, 'dist')
-      // const output = args.output ? (path.isAbsolute(args.output) ? args.output : path.resolve(userRoot, args.output)) : undefined
-      // const generateTsconfig = path.resolve(generateRoot, './tsconfig.json')
-      // const outdir = output || defaultOutput
+      await buildTypes()
 
-      // exec(`node ${tscBinRoot} --declaration --outDir ${outdir} --project ${generateTsconfig}`, generateRoot)
+      const defaultOutput = clientRoot ? path.resolve(clientRoot, 'build') : path.resolve(userRoot, 'dist')
+      const output = args.output ? (path.isAbsolute(args.output) ? args.output : path.resolve(userRoot, args.output)) : undefined
+      const generateTsconfig = path.resolve(generateRoot, './tsconfig.json')
+      const outdir = output || defaultOutput
 
-      // const dir = path.resolve(outdir, '../')
-      // const log = dir.endsWith('@harsta/client')
-      //   ? '@harsta/client'
-      //   : path.dirname(dir)
-      // consola.log(`\n✔ Generated Harsta Client ${dim(`to ${log}`)}\n`)
+      exec(`node ${tscBinRoot} --declaration --outDir ${outdir} --project ${generateTsconfig}`, generateRoot)
 
-      // async function buildAddresses() {
-      //   const packAddressesPath = path.resolve(generateRoot, './addresses/index.ts')
-      //   const addresses = await resolveUserAddresses()
-      //   await fs.ensureDir(path.dirname(packAddressesPath))
-      //   await fs.writeFile(packAddressesPath, addresses)
-      // }
+      const dir = path.resolve(outdir, '../')
+      const log = dir.endsWith('@harsta/client')
+        ? '@harsta/client'
+        : path.dirname(dir)
+      consola.log(`\n✔ Generated Harsta Client ${dim(`to ${log}`)}\n`)
 
-      // async function buildContractFactories(
-      //   paths: typeof fragmentsExtendsPaths,
-      //   outdir: string,
-      //   typechainsPath: string,
-      //   suffixRows: string[] = [],
-      // ) {
-      //   await fs.ensureDir(outdir)
+      async function buildAddresses() {
+        const packAddressesPath = path.resolve(generateRoot, './addresses/index.ts')
+        const addresses = await resolveUserAddresses()
+        await fs.ensureDir(path.dirname(packAddressesPath))
+        await fs.writeFile(packAddressesPath, addresses)
+      }
 
-      //   const indexRows: string[] = []
+      async function buildContractFactories(
+        paths: typeof fragmentsExtendsPaths,
+        outdir: string,
+        typechainsPath: string,
+        suffixRows: string[] = [],
+      ) {
+        await fs.ensureDir(outdir)
 
-      //   for (const { outfile, input } of paths) {
-      //     const { name, dirname, file } = outfile
-      //     const fileRows = [
-      //       `import { ${name}__factory } from '${path.relative(dirname, typechainsPath)}'`,
-      //       `import { resolveAddress, resolveRunner } from '${path.relative(dirname, presolve('./utils'))}'`,
-      //       `import type { Runner } from '${path.relative(dirname, presolve('./types'))}'`,
-      //       `import { ${name}, ${name}Interface } from '${input.import}'`,
-      //       '',
-      //       `export type { ${name}, ${name}Interface }`,
-      //       '',
-      //       `export class ${name}Factory {`,
-      //       `  static abi = ${name}__factory.abi`,
-      //       '',
-      //       `  static interface(): ${name}Interface {`,
-      //       `    return ${name}__factory.createInterface()`,
-      //       '  }',
-      //       '',
-      //       `  static attach(address: string, runner?: Runner): ${name} {`,
-      //       `    return ${name}__factory.connect(address, resolveRunner(runner))`,
-      //       '  }',
-      //       '',
-      //       `  static resolve(runner?: Runner, address?: string): ${name} {`,
-      //       `    const resolvedRunner = resolveRunner(runner)`,
-      //       `    const target = address || resolveAddress(this.name, resolvedRunner)`,
-      //       `    return ${name}__factory.connect(target, resolvedRunner)`,
-      //       `  }`,
-      //       '}',
-      //     ]
-      //     await fs.ensureDir(dirname)
-      //     await fs.writeFile(file, fileRows.join('\n'))
-      //     const exportFile = file.replace('.ts', '')
-      //     const exportPath = path.relative(outdir, exportFile)
-      //     indexRows.push(`export { ${name}Factory as ${name} } from './${exportPath.replace(/\\/g, '/')}'`)
-      //   }
+        const indexRows: string[] = []
 
-      //   indexRows.push(...suffixRows)
+        for (const { outfile, input } of paths) {
+          const { name, dirname, file } = outfile
+          const fileRows = [
+            `import { ${name}__factory } from '${path.relative(dirname, typechainsPath)}'`,
+            `import { resolveAddress, resolveRunner } from '${path.relative(dirname, presolve('./utils'))}'`,
+            `import type { Runner } from '${path.relative(dirname, presolve('./types'))}'`,
+            `import { ${name}, ${name}Interface } from '${input.import}'`,
+            '',
+            `export type { ${name}, ${name}Interface }`,
+            '',
+            `export class ${name}Factory {`,
+            `  static abi = ${name}__factory.abi`,
+            '',
+            `  static interface(): ${name}Interface {`,
+            `    return ${name}__factory.createInterface()`,
+            '  }',
+            '',
+            `  static attach(address: string, runner?: Runner): ${name} {`,
+            `    return ${name}__factory.connect(address, resolveRunner(runner))`,
+            '  }',
+            '',
+            `  static resolve(runner?: Runner, address?: string): ${name} {`,
+            `    const resolvedRunner = resolveRunner(runner)`,
+            `    const target = address || resolveAddress(this.name, resolvedRunner)`,
+            `    return ${name}__factory.connect(target, resolvedRunner)`,
+            `  }`,
+            '}',
+          ]
+          await fs.ensureDir(dirname)
+          await fs.writeFile(file, fileRows.join('\n'))
+          const exportFile = file.replace('.ts', '')
+          const exportPath = path.relative(outdir, exportFile)
+          indexRows.push(`export { ${name}Factory as ${name} } from './${exportPath.replace(/\\/g, '/')}'`)
+        }
 
-      //   indexRows.push('')
+        indexRows.push(...suffixRows)
 
-      //   await fs.writeFile(path.resolve(outdir, './index.ts'), indexRows.join('\n'))
-      // }
+        indexRows.push('')
 
-      // async function buildChains() {
-      //   const chainsDir = presolve('./chains')
-      //   await fs.ensureDir(chainsDir)
-      //   const indexRows: string[] = []
-      //   for (const alias in userConf.networks) {
-      //     const network = userConf.networks[alias]
-      //     if (!network)
-      //       return
-      //     const chain: Chain = {
-      //       id: network.id,
-      //       name: network.name,
-      //       nativeCurrency: network.currency,
-      //       rpcUrls: {
-      //         default: { http: [network.rpc] },
-      //         public: { http: [network.rpc] },
-      //       },
-      //       ...(network.explorer
-      //         ? { blockExplorers: { default: network.explorer } }
-      //         : {}),
-      //       iconUrl: network?.icon,
-      //       testnet: network.testnet,
-      //     }
-      //     indexRows.push(`export const ${alias} = ${JSON.stringify(chain, null, 2)} as const\n`)
-      //   }
-      //   if (!indexRows.length)
-      //     indexRows.push('export {}')
-      //   await fs.writeFile(path.resolve(chainsDir, './index.ts'), indexRows.join('\n'))
-      // }
+        await fs.writeFile(path.resolve(outdir, './index.ts'), indexRows.join('\n'))
+      }
 
-      // async function buildFragments() {
-      //   const indexRows: string[] = []
+      async function buildChains() {
+        const chainsDir = presolve('./chains')
+        await fs.ensureDir(chainsDir)
+        const indexRows: string[] = []
+        for (const alias in userConf.networks) {
+          const network = userConf.networks[alias]
+          if (!network)
+            return
+          const chain: Chain = {
+            id: network.id,
+            name: network.name,
+            nativeCurrency: network.currency,
+            rpcUrls: {
+              default: { http: [network.rpc] },
+              public: { http: [network.rpc] },
+            },
+            ...(network.explorer
+              ? { blockExplorers: { default: network.explorer } }
+              : {}),
+            iconUrl: network?.icon,
+            testnet: network.testnet,
+          }
+          indexRows.push(`export const ${alias} = ${JSON.stringify(chain, null, 2)} as const\n`)
+        }
+        if (!indexRows.length)
+          indexRows.push('export {}')
+        await fs.writeFile(path.resolve(chainsDir, './index.ts'), indexRows.join('\n'))
+      }
 
-      //   for (const { outfile, path } of fragmentsPaths) {
-      //     indexRows.push(`export { default as ${outfile.name}Fragment } from './${path}'`)
-      //   }
-      //   if (!indexRows.length)
-      //     indexRows.push('export {}')
-      //   indexRows.push('')
-      //   await fs.ensureDir(presolve('./fragments'))
-      //   if (userConf.paths?.fragments) {
-      //     await fs.copy(
-      //       path.resolve(generateRoot, './fragments'),
-      //       resolveUserPath(userConf.paths.fragments)!,
-      //     )
-      //   }
-      //   await fs.writeFile(path.resolve(presolve('./fragments'), './index.ts'), indexRows.join('\n'))
-      // }
+      async function buildFragments() {
+        const indexRows: string[] = []
 
-      // async function buildContracts() {
-      //   await buildContractFactories(
-      //     fragmentsPaths,
-      //     presolve('./contracts'),
-      //     presolve('./typechains'),
-      //     ['', `export * from './extends'`],
-      //   )
-      // }
+        for (const { outfile, path } of fragmentsPaths) {
+          indexRows.push(`export { default as ${outfile.name}Fragment } from './${path}'`)
+        }
+        if (!indexRows.length)
+          indexRows.push('export {}')
+        indexRows.push('')
+        await fs.ensureDir(presolve('./fragments'))
+        if (userConf.paths?.fragments) {
+          await fs.copy(
+            path.resolve(generateRoot, './fragments'),
+            resolveUserPath(userConf.paths.fragments)!,
+          )
+        }
+        await fs.writeFile(path.resolve(presolve('./fragments'), './index.ts'), indexRows.join('\n'))
+      }
 
-      // async function buildContractsExtends() {
-      //   if (!fragmentsExtendsPaths.length) {
-      //     await fs.ensureDir(presolve('./contracts/extends'))
-      //     await fs.writeFile(presolve('./contracts/extends/index.ts'), 'export {}\n')
-      //     return
-      //   }
+      async function buildContracts() {
+        await buildContractFactories(
+          fragmentsPaths,
+          presolve('./contracts'),
+          presolve('./typechains'),
+          ['', `export * from './extends'`],
+        )
+      }
 
-      //   const allFiles = glob(userRoot, ['./config/fragments/*.json'])
-      //   await runTypeChain({
-      //     cwd: userRoot,
-      //     allFiles,
-      //     filesToProcess: allFiles,
-      //     target: 'ethers-v6',
-      //     outDir: presolve('./typechains/extends'),
-      //   })
+      async function buildContractsExtends() {
+        if (!fragmentsExtendsPaths.length) {
+          await fs.ensureDir(presolve('./contracts/extends'))
+          await fs.writeFile(presolve('./contracts/extends/index.ts'), 'export {}\n')
+          return
+        }
 
-      //   await buildContractFactories(
-      //     fragmentsExtendsPaths,
-      //     presolve('./contracts/extends'),
-      //     presolve('./typechains/extends'),
-      //   )
-      // }
+        await buildContractFactories(
+          fragmentsExtendsPaths,
+          presolve('./contracts/extends'),
+          presolve('./typechains/extends'),
+        )
+      }
 
-      // async function buildTypes() {
-      //   const paths = [...fragmentsPaths, ...fragmentsExtendsPaths] as typeof fragmentsPaths
-      //   const types = [
-      //     {
-      //       type: 'events',
-      //       filter: (mod: string) => mod.endsWith('Event'),
-      //       outfile: './events/index.ts',
-      //     },
-      //     {
-      //       type: 'interfaces',
-      //       filter: (mod: string) => mod.endsWith('Interface'),
-      //       outfile: './interfaces/index.ts',
-      //     },
-      //     {
-      //       type: 'instances',
-      //       filter: (mod: string, name: string) => mod === name,
-      //       outfile: './instances/index.ts',
-      //     },
-      //   ]
+      async function buildTypes() {
+        const paths = [...fragmentsPaths, ...fragmentsExtendsPaths] as typeof fragmentsPaths
+        const types = [
+          {
+            type: 'events',
+            filter: (mod: string) => mod.endsWith('Event'),
+            outfile: './events/index.ts',
+          },
+          {
+            type: 'interfaces',
+            filter: (mod: string) => mod.endsWith('Interface'),
+            outfile: './interfaces/index.ts',
+          },
+          {
+            type: 'instances',
+            filter: (mod: string, name: string) => mod === name,
+            outfile: './instances/index.ts',
+          },
+        ]
 
-      //   for (const config of types) {
-      //     const outfile = path.resolve(generateRoot, config.outfile)
-      //     const dirname = path.dirname(outfile)
+        for (const config of types) {
+          const outfile = path.resolve(generateRoot, config.outfile)
+          const dirname = path.dirname(outfile)
 
-      //     await fs.remove(dirname)
-      //     await fs.ensureDir(dirname)
+          await fs.remove(dirname)
+          await fs.ensureDir(dirname)
 
-      //     function resolve({ input, outfile: { name } }: typeof paths[number]) {
-      //       const exports = input.exports.filter(mod => config.filter(mod, name)) || []
-      //       const importPath = `${path.relative(dirname, input.typechains)}/${input.relative}`
-      //       return `export type { ${exports.join(', ')} } from '${importPath}'`
-      //     }
-      //     if (config.type !== 'events') {
-      //       const rows = paths.map(resolve)
-      //       await fs.writeFile(outfile, rows.join('\n'))
-      //       continue
-      //     }
-      //     for (const p of paths) {
-      //       const outfile = path.resolve(dirname, `${p.outfile.name}.ts`)
-      //       await fs.writeFile(outfile, resolve(p))
-      //     }
-      //     const indexRows = paths
-      //       .map(p => p.outfile.name)
-      //       .map(name => `export * as ${name} from './${name}'`)
-      //     await fs.writeFile(outfile, indexRows.join('\n'))
-      //   }
-      // }
+          function resolve({ input, outfile: { name } }: typeof paths[number]) {
+            const exports = input.exports.filter(mod => config.filter(mod, name)) || []
+            const importPath = `${path.relative(dirname, input.typechains)}/${input.relative}`
+            return `export type { ${exports.join(', ')} } from '${importPath}'`
+          }
+          if (config.type !== 'events') {
+            const rows = paths.map(resolve)
+            await fs.writeFile(outfile, rows.join('\n'))
+            continue
+          }
+          for (const p of paths) {
+            const outfile = path.resolve(dirname, `${p.outfile.name}.ts`)
+            await fs.writeFile(outfile, resolve(p))
+          }
+          const indexRows = paths
+            .map(p => p.outfile.name)
+            .map(name => `export * as ${name} from './${name}'`)
+          await fs.writeFile(outfile, indexRows.join('\n'))
+        }
+      }
     },
   )
 }

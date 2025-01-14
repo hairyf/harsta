ba9b54b1c22fa5f2481e66fb5b647186aa3f719e
Mr.Mao <951416545@qq.com>
2025-01-14 14:07:26 +0800

    chore: update utils package

--- packages/utils/src/number.ts
+++ packages/utils/src/number.ts
@@ -1,14 +1,9 @@
-import BigNumber from 'bignumber.js'
 import { formatEther as _formatEther } from 'ethers'
+import type { Numberish } from '@hairy/format'
+import { formatNumeric, numerfix, unum } from '@hairy/format'
+import BigNumber from 'bignumber.js'
 
-export function numerfix(value: any) {
-  const _isNaN = isNaN(Number(value)) || value.toString() === 'NaN'
-  if (_isNaN)
-    console.warn(`numerfix(${value}): value is not the correct value. To ensure the normal operation of the program, it will be converted to zero`)
-  return (_isNaN) ? '0' : String(value)
-}
-
-
-export function formatEther(value: any = '0') {
-  return _formatEther(new BigNumber(numerfix(value)).toFixed(0))
+export function formatEther(value: Numberish = '0', decimalPlaces = 2, roundingMode = BigNumber.ROUND_DOWN) {
+  const num = _formatEther(unum(numerfix(value)).toFixed(0))
+  return formatNumeric(num, { d: decimalPlaces, r: roundingMode })
 }

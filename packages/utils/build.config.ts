d4ee29a61320d0dc0294249fa5984ca42777ce8b
Mr.Mao <951416545@qq.com>
2025-01-15 11:45:59 +0800

    chore: add utils build externals

--- packages/utils/build.config.ts
+++ packages/utils/build.config.ts
@@ -7,6 +7,7 @@ export default defineBuildConfig({
   declaration: true,
   clean: true,
   failOnWarn: false,
+  externals: ['@hairy/format', 'ethers', 'mitt'],
   rollup: {
     emitCJS: true,
   },

84ae732d8760a11196dec844555603a00b061c72
Mr.Mao <951416545@qq.com>
2025-01-15 11:42:07 +0800

    chore: lint fix

--- packages/utils/src/util.ts
+++ packages/utils/src/util.ts
@@ -2,7 +2,7 @@ export function isString(value: any): value is string {
   return typeof value === 'string'
 }
 export function isBoolean(value: any): value is boolean {
-  return typeof value ==='boolean'
+  return typeof value === 'boolean'
 }
 
 export function cover(value: string, mode: [number, number, number], symbol = '*') {

88737e8d5cf5f740c372fdf0aa83234581c6ee1f
hairyf <wwu710632@gmail.com>
2025-02-28 14:19:04 +0800

    fix: remove events error

--- packages/harsta/generated/index.ts
+++ packages/harsta/generated/index.ts
@@ -1,7 +1,5 @@
 export * as typechain from './_typechain-contracts'
 export * as fragments from './_fragments-contracts'
-
-export * as Events from './events'
 export * as factories from './factories'
 export * as contracts from './contracts'
 export * as chains from './chains'
